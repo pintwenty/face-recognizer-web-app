06d76d486fe589b72429aeadb30cfac1b8460e50
pintwenty <santhoshsai20@gmail.com>
2019-08-26 19:37:35 +0530

    index changed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "tachyons";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render( < App / > , document.getElementById("root"));
 
 serviceWorker.unregister();
 
@@ -19,3 +19,11 @@ express()
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))*/
+var http = require('http')
+var server = http.createServer((function (request, response) {
+    response.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+    response.end("Hello World\n");
+}));
+server.listen(3000);
