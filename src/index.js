8fb258aa53d7ade5e0f10aff5f5f9d0d0068e18b
pintwenty <santhoshsai20@gmail.com>
2019-08-26 21:08:56 +0530

    a

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from "react";
+/*import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "tachyons";
+import "tachyons";*/
+require("react");
+require("react-dom");
+require("./index.css");
+require("./App");
+require("./serviceWorker");
+require("tachyons");
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
