db9fd1a2c9bade6fecc312f173f466dc2a188c36
pintwenty <santhoshsai20@gmail.com>
2019-08-26 20:35:22 +0530

    edited

--- src/index.js
+++ src/index.js
@@ -5,18 +5,22 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "tachyons";
 
-ReactDOM.render( < App / > , document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
 
 //port setting
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 8080
+const http = require("http");
 
-express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World\n");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
