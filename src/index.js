ddd47f45583b67379c8674bb14b33abbe8ce96f5
pintwenty <santhoshsai20@gmail.com>
2019-08-26 19:15:46 +0530

    First commit

--- src/index.js
+++ src/index.js
@@ -5,20 +5,17 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "tachyons";
 
-ReactDOM.render( < App / > , document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 8080
+/*const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 8080;
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))*/
