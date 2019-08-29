efcdb26d005fa3b31f03313976b0067fc2c676fc
pintwenty <santhoshsai20@gmail.com>
2019-08-29 15:12:29 +0530

    index edited

--- src/index.js
+++ src/index.js
@@ -5,18 +5,6 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "tachyons";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render( < App / > , document.getElementById("root"));
 
 serviceWorker.unregister();
-
-//port setting
-const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 5000;
-
-express()
-  .use(express.static(path.join(__dirname, "public")))
-  .set("views", path.join(__dirname, "views"))
-  .set("view engine", "ejs")
-  .get("/", (req, res) => res.render("pages/index"))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
